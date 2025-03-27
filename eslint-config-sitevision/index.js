35bb28213d24007d38ecc2d7895798e8d2b1d37d
robertjohanson93 <robert.johanson.93@gmail.com>
2025-03-27 12:27:32 +0100

    feat: add webperf rules

--- eslint-config-sitevision/index.js
+++ eslint-config-sitevision/index.js
@@ -24,6 +24,25 @@ module.exports = {
     "no-empty-function": 2,
     "func-style": ["error", "expression"],
     "vars-on-top": 2,
-    "no-var": 2
+    "no-var": 2,
+    // Webperf additions
+    "no-eval": "error",
+    "no-implied-eval": "error",
+    "no-new-func": "error",
+    "no-script-url": "error",
+    "no-unsafe-finally": "error",
+    "no-unsafe-negation": "error",
+    "no-prototype-builtins": "error",
+    "no-self-assign": "error",
+    "no-self-compare": "error",
+    "no-unmodified-loop-condition": "error",
+    "no-unused-expressions": "error",
+    "no-useless-call": "error",
+    "no-useless-concat": "error",
+    "no-useless-escape": "error",
+    "no-with": "error",
+    "require-await": "error",
+    "no-return-await": "error",
+    "eqeqeq": ["error", "always"]
   }
 };
